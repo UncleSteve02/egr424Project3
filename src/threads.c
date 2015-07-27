549d8430ac327c00583551fe173183859017ef15
Steven Demers <sjdemers@gmail.com>
2015-07-26 20:25:02 -0400

    Implemented functions: thread2_LED and thread3_OLED.

--- src/threads.c
+++ src/threads.c
@@ -2,6 +2,9 @@
 #include "scheduler.h"
 #include "lock.h"
 
+// BIT BANDING - Alias address of GPIO-PORTF_DATA_R
+#define LED_BB *((volatile unsigned int *) 0x424A7F80)
+
 void thread1_UART(void)
 {
   while (1) {
@@ -21,12 +24,27 @@ void thread1_UART(void)
 
 void thread2_LED(void)
 {
-
+  volatile unsigned ulLoop;
+  while (1) {
+    // Delay for a bit.
+    for(ulLoop = 0; ulLoop < 200000; ulLoop++) {}
+    // Toggle LED
+    LED_BB ^= 1;
+  }
 }
 
 void thread3_OLED(void)
 {
-
+  volatile unsigned ulLoop;
+  while (1) {
+    // Clear OLED screen
+    RIT128x96x4Clear();
+    // Delay for a bit.
+    for(ulLoop = 0; ulLoop < 200000; ulLoop++) {}
+    RIT128x96x4StringDraw(string, 5, 50, 5);
+    for(ulLoop = 0; ulLoop < 200000; ulLoop++) {}
+    yield();
+  }
 }
 
 void thread4_UART(void)
